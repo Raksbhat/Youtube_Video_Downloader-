1ecbf17991df6ece576ba8c1c6be7e48ae8a399f
Raksbhat <95375478+Raksbhat@users.noreply.github.com>
2022-08-01 10:23:26 +0530

    Update Youtbe video downloader.py

--- Youtbe video downloader.py
+++ Youtbe video downloader.py
@@ -1,10 +1,10 @@
 from pytube import YouTube
 #paste your yt video link here below in link section
-link="https://youtu.be/WNeLUngb-Xg"
+link=" "
 yt=YouTube(link)
 #printing Title of the video
 print("Title: ",yt.title)
 #Downloading the video
 yd= yt.streams.get_highest_resolution()
-yd.download()
+yd.download(#specify the download path)
 print("Thank you for using this software")
